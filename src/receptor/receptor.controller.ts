1f95518b62d26e45d385aec3c555acb611e73b9c
Diego Saggioro <diegosaggiorobrito@gmail.com>
2025-06-16 23:07:23 -0300

    adicionado middleware de autenticacao para receptor

--- src/receptor/receptor.controller.ts
+++ src/receptor/receptor.controller.ts
@@ -6,36 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ReceptorService } from './receptor.service';
 import { CreateReceptorDto } from './dto/create-receptor.dto';
 import { UpdateReceptorDto } from './dto/update-receptor.dto';
 import { Receptor } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('receptor')
 export class ReceptorController {
   constructor(private readonly receptorService: ReceptorService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() data: CreateReceptorDto) {
     return this.receptorService.create(data);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.receptorService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.receptorService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReceptorDto: Receptor) {
     return this.receptorService.update(updateReceptorDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.receptorService.remove(+id);
